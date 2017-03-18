f854f9bd93c9fce18e9f91b269aa08290ea9910a
Logan Bestwick <loganbestwick@gmail.com>
2017-03-18 12:46:31 -0700

    hello world

--- server/main.go
+++ server/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"goji.io"
+	"goji.io/pat"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "name")
+	fmt.Fprintf(w, "hello, %s!", name)
+}
+
+func main() {
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+
+	http.ListenAndServe("localhost:8000", mux)
 }
