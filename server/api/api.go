812ef6bc8f6b7f4cfe86cbd55c77ad48df45750e
Logan Bestwick <loganbestwick@gmail.com>
2017-04-09 11:12:10 -0700

    hello world route, starting to implement DB

--- server/api/api.go
+++ server/api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"goji.io/pat"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "name")
+	fmt.Fprintf(w, "hello, %s!", name)
+}
--- server/main.go
+++ server/main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"database/sql"
+	"encoding/json"
 
 	"goji.io"
 	"goji.io/pat"
+	"github.com/loganbestwick/kula/server/api"
+	_ "github.com/lib/pq"
+	"os"
+	"fmt"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	name := pat.Param(r, "name")
-	fmt.Fprintf(w, "hello, %s!", name)
+type DBConfig struct {
+	Host string
+	Port int
+	User string
+	DBName string
+}
+
+func getDBConfig() DBConfig {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	config := &DBConfig{}
+	err := decoder.Decode(config)
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil
+	}
+	return config
+}
+
+func setupDB(config *DBConfig) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.DBName)
 }
 
 func main() {
+	config := getDBConfig()
+	setupDB(config)
 	mux := goji.NewMux()
-	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+	mux.HandleFunc(pat.Get("/v1/:workout"), api.Hello)
 
 	http.ListenAndServe("localhost:8000", mux)
 }
